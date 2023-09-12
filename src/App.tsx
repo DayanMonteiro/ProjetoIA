2219778537d143002c7e765c240d813d421e09ab
DayanMonteiro <dmsmamb@gmail.com>
2023-09-12 13:29:37 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "./components/ui/button";
 import { Github } from "lucide-react";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -11,12 +12,29 @@ export function App() {
           <span className="text-sm text-muted-foreground">
             Desenvolvido nas horas vagas
           </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
           <Button variant={"outline"}>
             <Github className="w-4 h-4 mr-2" />
             GitHub
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1"></div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar a transcrição do seu vídeo selecionado.
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   );
 }
+
+// parei em 40:51 falta roda o comando npm dlx shadcn-ui@latest add separator
