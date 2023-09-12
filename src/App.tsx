c10bb45548402d92f3912c8765b146bcea9d1ae8
DayanMonteiro <dmsmamb@gmail.com>
2023-09-12 09:27:36 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+
 export function App() {
   return (
     <div>
-      <h1>HUE</h1>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido nas horas vagas
+          </span>
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
